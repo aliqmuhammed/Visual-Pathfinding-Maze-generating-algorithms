4d1a5831325e5425818d98b3b462bd0d1035aec3
Ali Muhammed <Alawi93@live.se>
2020-12-25 18:11:54 +0100

    added depth first search

---
+++ src/visualize/algorithms/pathfindingAlgorithms/dfs.js
@@ -0,0 +1,82 @@
+
+
+var visited = [];
+//visited nodes in order
+var visitedNodesInOrder = [];
+export default function init(maze,node){
+  //  visited =[maze.length][maze[0].length]
+
+  visitedNodesInOrder = [];
+  
+
+   
+  
+    var state =  dfs(maze,node);
+    console.log('state: ' + state)
+    if(state){
+        return visitedNodesInOrder;
+    }else if (!state){
+        if(visitedNodesInOrder === 0){
+            return 'start position is completly blocked'
+        }
+        return visitedNodesInOrder
+    }
+    
+}
+function dfs(maze, node){
+    
+    if (maze[node.row][node.col].isFinish){
+       console.log("FOUND IT AT: ROW: " + node.row + " COL: " + node.col );
+        return true;
+    }
+
+    var neighbours = getNeighbors(maze,node);
+    //typeof(neighbors) === 'undefined' ||
+    if (neighbours.length <= 0){
+        return false;
+    }
+    for (var i=0;i<neighbours.length;i++){
+        console.log('Visited: ' + "(" + node.row + "," + node.col + ")");
+        
+        maze[neighbours[i].row][neighbours[i].col].isVisited = true;
+        console.log()
+        if(dfs(maze,neighbours[i])){
+            return true;
+        }
+    }
+    return false;
+}
+//directions
+const dr = [-1,+1,0,0];
+const dc = [0,0,+1,-1];
+
+function getNeighbors(grid,node){
+  var neighbours = [];
+    //checks all direction from the node added
+    for (let i = 0; i < 4; i++) {
+        var   newRow = node.row + dr[i];
+        var   newCol = node.col + dc[i];
+        
+           //if the column or row is outside the grid then we stop iterating
+           if(newCol < 0 || newCol >= grid[0].length|| newRow < 0 || newRow >= grid.length){
+                 continue;
+             }
+
+             
+             //checking that the node has not already been visited else we stop iterating
+             if (grid[newRow][newCol].isVisited){
+                 continue;
+             }
+
+             if(grid[newRow][newCol].isWall){
+                 continue;
+             }
+             //push valid neighbours from current node
+             visitedNodesInOrder.push({row: node.row,col: node.col})
+             neighbours.push({row: newRow,col: newCol})
+  }
+return neighbours;
+}
+
+
+
